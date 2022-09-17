2a4adf38d558290ea4172ae3cfc9364c60fc40a6
tanaka0625 <tanasou6@gmail.com>
2022-09-17 22:31:15 +0900

    遅マックでフッター表示されないバグ修正

--- resources/views/Grouped_answers/index.blade.php
+++ resources/views/Grouped_answers/index.blade.php
@@ -93,7 +93,8 @@
             @continue;
         @else
 
-            <x-answer :text='$item->text' :maker='$item->getMaker()' :like='$item->like' :vote='$item->vote' :questionText='$item->getQuestionText()' :questionId='$item->getQuestionId()' btnType="like" :likeUserNames='$item->getLikeUserNames()' :userId='$item->user_id' :questionSituation='App\Models\Question::find($item->question_id)->getSituation()' kind='$item->kind'>
+            <x-answer :text='$item->text' :maker='$item->getMaker()' :like='$item->like' :vote='$item->vote' :questionText='$item->getQuestionText()' :questionId='$item->getQuestionId()' btnType="like" :likeUserNames='$item->getLikeUserNames()' :userId='$item->user_id' :questionSituation='App\Models\Question::find($item->question_id)->getSituation()' :kind='$item->kind'>
+                {{$item->created_at}}
             </x-answer>
         @endif
     @endforeach
