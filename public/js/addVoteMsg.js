eb412eadf5002b96b29bb2ffb78737a5f488238e
tanaka0625 <tanasou6@gmail.com>
2022-09-23 03:15:38 +0900

    vote-msgが無い時のエラー修正

--- public/js/addVoteMsg.js
+++ public/js/addVoteMsg.js
@@ -4,7 +4,7 @@ for(let i=0; i<likeUsers.length; i++){
     if(typeof likeUsers[i]["vote"] != "undefined"){
 
         for(let x=0; x<likeUsers[i]["vote"].length; x++){
-            if(likeUsers[i]["vote"][x]["id"] == userId){
+            if(likeUsers[i]["vote"][x]["id"] == userId && item[i].getElementsByClassName('vote-msg').length > 0){
                 item[i].getElementsByClassName('vote-msg')[0].classList.add('on');
             }
         }
